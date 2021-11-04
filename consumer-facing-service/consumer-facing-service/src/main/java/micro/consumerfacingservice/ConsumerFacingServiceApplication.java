991bf0d20667929a971e038d43f32c04ff953d0f
ABHIJIT PATRA <abhijitpatra337@gmail.com>
2021-11-04 11:43:38 +0530

    Add files via upload

---
+++ consumer-facing-service/consumer-facing-service/src/main/java/micro/consumerfacingservice/ConsumerFacingServiceApplication.java
@@ -0,0 +1,13 @@
+package micro.consumerfacingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerFacingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerFacingServiceApplication.class, args);
+	}
+
+}
---
+++ consumer-facing-service/consumer-facing-service/src/test/java/micro/consumerfacingservice/ConsumerFacingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.consumerfacingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerFacingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
